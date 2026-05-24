7f561cde86ea78416875ceff0ad3a209c9987e0b
dinethchatumina2011 <dinethchatumina847@gmail.com>
2026-05-23 19:19:37 -0700

    Create  config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    // Bot Settings - මෙතනින් වෙනස් කරන්න පුළුවන්
+    botName: "CR MINI BOT",
+    ownerName: "REHAN",
+    ownerNumber: "94741336839", // ඔයාගේ number එක 94 අගයෙන්
+    prefix: ".",
+    botImage: "./assets/bot.jpg", // bot image path
+
+    // Pair Site Settings
+    pairPort: process.env.PORT || 8000,
+
+    // Channel React Settings
+    channelId: "120363xxxxxxx@newsletter", // ඔයාගේ channel id
+    reactEmoji: "🔥❤️😂" // react වෙන්න ඕන emoji
+}
